d36c67e8618ffeb095b8e55dbb054cad9d0a58e9
Mjunaid65 <mjunaidahmed3@gmail.com>
2025-07-21 23:59:23 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+
+with open('heart_failure_model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+with open('scaler.pkl', 'rb') as scaler_file:
+    scaler = pickle.load(scaler_file)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        
+        model_features = [
+            float(request.form['age']),
+            float(request.form['anaemia']),
+            float(request.form['creatinine_phosphokinase']),
+            float(request.form['diabetes']),
+            float(request.form['ejection_fraction']),
+            float(request.form['high_blood_pressure']),
+            float(request.form['platelets']),
+            float(request.form['serum_creatinine']),
+            float(request.form['serum_sodium']),
+            float(request.form['sex']),
+            float(request.form['smoking']),
+            float(request.form['time'])
+        ]
+
+        
+        input_data = np.array([model_features])
+        input_scaled = scaler.transform(input_data)
+        prediction = model.predict(input_scaled)[0]
+
+        result = "High Risk of Death" if prediction == 1 else "Low Risk of Death"
+
+        return render_template('index.html', result=result)
+    except Exception as e:
+        return render_template('index.html', result=f"Error: {str(e)}")
+
+if __name__ == '__main__':
+    app.run(debug=True)
